44838b8543fd2f1de856335702e4ebcb237372d8
stianrbr <stianrbr@stud.ntnu.no>
2022-10-06 16:09:39 +0200

    Monday 18.00

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ viv_0_2_n1.plot_all_timeseries()
 viv_0_2_n1.plot_section_timeseries()
 viv_0_2_n1_time = viv_0_2_n1.measurements.Channels[0]
 
+
 ##
 # VIV velocity 0.3 m/s - 1st run
 viv_0_3_n1 = VIV_files("viv_0_3_n1.bin", starttimes=10.1, endtimes=65.2, title="Velocity: 0.3 m/s")
@@ -38,49 +39,49 @@ viv_0_4_n1.plot_section_timeseries()
 
 ##
 # VIV velocity 0.5 m/s - 1st run
-viv_0_5_n1 = VIV_files("viv_0_5_n1.bin", starttimes=12.2, endtimes=44.5, title="Velocity: 0.5 m/s")
+viv_0_5_n1 = VIV_files("viv_0_5_n1.bin", starttimes=12, endtimes=44.5, title="Velocity: 0.5 m/s")
 viv_0_5_n1.write_statistics()
 viv_0_5_n1.plot_all_timeseries()
 viv_0_5_n1.plot_section_timeseries()
 
 ##
 # VIV velocity 0.6 m/s - 1st run
-viv_0_6_n1 = VIV_files("viv_0_6_n1.bin", starttimes=15, endtimes=39.7, title="Velocity: 0.6 m/s")
+viv_0_6_n1 = VIV_files("viv_0_6_n1.bin", starttimes=20, endtimes=39.7, title="Velocity: 0.6 m/s")
 viv_0_6_n1.write_statistics()
 viv_0_6_n1.plot_all_timeseries()
 viv_0_6_n1.plot_section_timeseries()
 
 ##
 # VIV velocity 0.7 m/s - 1st run
-viv_0_7_n1 = VIV_files("viv_0_7_n1.bin", starttimes=13.9, endtimes=35.4, title="Velocity: 0.7 m/s")
+viv_0_7_n1 = VIV_files("viv_0_7_n1.bin", starttimes=18, endtimes=35.4, title="Velocity: 0.7 m/s")
 viv_0_7_n1.write_statistics()
 viv_0_7_n1.plot_all_timeseries()
 viv_0_7_n1.plot_section_timeseries()
 
 ##
 # VIV velocity 0.75 m/s - 1st run
-viv_0_75_n1 = VIV_files("viv_0_75_n1.bin", starttimes=14.0, endtimes=33.62, title="Velocity: 0.75 m/s")
+viv_0_75_n1 = VIV_files("viv_0_75_n1.bin", starttimes=17.0, endtimes=33.62, title="Velocity: 0.75 m/s")
 viv_0_75_n1.write_statistics()
 viv_0_75_n1.plot_all_timeseries()
 viv_0_75_n1.plot_section_timeseries()
 
 ##
 # VIV velocity 0.8 m/s - 1st run
-viv_0_8_n1 = VIV_files("viv_0_8_n1.bin", starttimes=14.9, endtimes=33.5, title="Velocity: 0.8 m/s")
+viv_0_8_n1 = VIV_files("viv_0_8_n1.bin", starttimes=19, endtimes=33.5, title="Velocity: 0.8 m/s")
 viv_0_8_n1.write_statistics()
 viv_0_8_n1.plot_all_timeseries()
 viv_0_8_n1.plot_section_timeseries()
 
 ##
 # VIV velocity 0.83 m/s - 1st run
-viv_0_83_n1 = VIV_files("viv_0_83_n1.bin", starttimes=13.9, endtimes=31.5, title="Velocity: 0.83 m/s")
+viv_0_83_n1 = VIV_files("viv_0_83_n1.bin", starttimes=19, endtimes=31.5, title="Velocity: 0.83 m/s")
 viv_0_83_n1.write_statistics()
 viv_0_83_n1.plot_all_timeseries()
 viv_0_83_n1.plot_section_timeseries()
 
 ##
 # VIV velocity 0.84 m/s - 1st run
-viv_0_84_n1 = VIV_files("viv_0_84_n1.bin", starttimes=12.7, endtimes=29.5, title="Velocity: 0.84 m/s")
+viv_0_84_n1 = VIV_files("viv_0_84_n1.bin", starttimes=18, endtimes=29.5, title="Velocity: 0.84 m/s")
 viv_0_84_n1.write_statistics()
 viv_0_84_n1.plot_all_timeseries()
 viv_0_84_n1.plot_section_timeseries()
@@ -97,9 +98,78 @@ viv_0_84_n1.FFT("2X", plot=True)
 viv_0_84_n1.FFT("3Y", plot=True)
 viv_0_84_n1.FFT("3X", plot=True)
 
+
+##
+# VIV velocity 0.84 m/s - 2nd run
+viv_0_84_n2 = VIV_files("viv_0_84_n2_001.bin", starttimes=21.5, endtimes=31.5, title="Velocity: 0.84 m/s")
+viv_0_84_n2.write_statistics()
+viv_0_84_n2.plot_all_timeseries()
+viv_0_84_n2.plot_section_timeseries()
+
+##
+# VIV velocity 0.84 m/s - 3rd run
+viv_0_84_n3 = VIV_files("viv_0_84_n3.bin", starttimes=22.5, endtimes=31, title="Velocity: 0.84 m/s")
+viv_0_84_n3.write_statistics()
+viv_0_84_n3.plot_all_timeseries()
+viv_0_84_n3.plot_section_timeseries()
+
+##
+# VIV velocity 0.84 m/s - 4th run
+viv_0_84_n4 = VIV_files("viv_0_84_n4.bin", starttimes=22, endtimes=32, title="Velocity: 0.84 m/s")
+viv_0_84_n4.write_statistics()
+viv_0_84_n4.plot_all_timeseries()
+viv_0_84_n4.plot_section_timeseries()
+
+##
+# VIV velocity 0.84 m/s - 5th run
+viv_0_84_n5 = VIV_files("viv_0_84_n5.bin", starttimes=23, endtimes=31, title="Velocity: 0.84 m/s")
+viv_0_84_n5.write_statistics()
+viv_0_84_n5.plot_all_timeseries()
+viv_0_84_n5.plot_section_timeseries()
+
+##
+
+# VIV velocity 0.84 m/s - 1st freq. change run
+viv_0_84_5hz = VIV_files("viv_0_84_5Hz_BE0_5Hz.bin", starttimes=20, endtimes=31, title="Velocity: 0.84 m/s", samplingfreq=5)
+viv_0_84_5hz.write_statistics()
+viv_0_84_5hz.plot_all_timeseries()
+viv_0_84_5hz.plot_section_timeseries()
+
+##
+
+# VIV velocity 0.84 m/s - 2nd freq. change run
+viv_0_84_10hz = VIV_files("viv_0_84_10Hz_BE1_25Hz.bin", starttimes=18, endtimes=31, title="Velocity: 0.84 m/s", samplingfreq=10)
+viv_0_84_10hz.write_statistics()
+viv_0_84_10hz.plot_all_timeseries()
+viv_0_84_10hz.plot_section_timeseries()
+
+##
+
+# VIV velocity 0.84 m/s - 3rd freq. change run
+viv_0_84_20hz = VIV_files("viv_0_84_20Hz_BE2_5Hz.bin", starttimes=23, endtimes=32, title="Velocity: 0.84 m/s", samplingfreq=20)
+viv_0_84_20hz.write_statistics()
+viv_0_84_20hz.plot_all_timeseries()
+viv_0_84_20hz.plot_section_timeseries()
+
+##
+
+# VIV velocity 0.84 m/s - 4th freq. change run
+viv_0_84_50 = VIV_files("viv_0_84_50Hz_BU5Hz.bin", starttimes=22, endtimes=32, title="Velocity: 0.84 m/s", samplingfreq=50)
+viv_0_84_50.write_statistics()
+viv_0_84_50.plot_all_timeseries()
+viv_0_84_50.plot_section_timeseries()
+
+##
+
+# VIV velocity 0.84 m/s - 5th freq. change run
+viv_0_84_100Hz = VIV_files("viv_0_84_100Hz_BU10Hz.bin", starttimes=18, endtimes=29.5, title="Velocity: 0.84 m/s", samplingfreq=100)
+viv_0_84_100Hz.write_statistics()
+viv_0_84_100Hz.plot_all_timeseries()
+viv_0_84_100Hz.plot_section_timeseries()
+
 ##
 # VIV velocity 0.85 m/s - 1st run
-viv_0_85_n1 = VIV_files("viv_0_85_n1.bin", starttimes=13.8, endtimes=31.4, title="Velocity: 0.85 m/s")
+viv_0_85_n1 = VIV_files("viv_0_85_n1.bin", starttimes=24, endtimes=31.4, title="Velocity: 0.85 m/s")
 viv_0_85_n1.write_statistics()
 viv_0_85_n1.plot_all_timeseries()
 viv_0_85_n1.plot_section_timeseries()
@@ -107,7 +177,7 @@ viv_0_85_n1.plot_section_timeseries()
 
 ##
 # VIV velocity 0.88 m/s - 1st run
-viv_0_88_n1 = VIV_files("viv_0_88_n1.bin", starttimes=15.1, endtimes=30.8, title="Velocity: 0.88 m/s")
+viv_0_88_n1 = VIV_files("viv_0_88_n1.bin", starttimes=23, endtimes=30.8, title="Velocity: 0.88 m/s")
 viv_0_88_n1.write_statistics()
 viv_0_88_n1.plot_all_timeseries()
 viv_0_88_n1.plot_section_timeseries()
@@ -122,7 +192,7 @@ viv_0_9_n1.plot_section_timeseries()
 
 ##
 # VIV velocity 1.0 m/s - 1st run
-viv_1_0_n1 = VIV_files("viv_1_0_n1.bin", starttimes=15.7, endtimes=29.4, title="Velocity: 1.0 m/s")
+viv_1_0_n1 = VIV_files("viv_1_0_n1.bin", starttimes=21, endtimes=29.4, title="Velocity: 1.0 m/s")
 viv_1_0_n1.write_statistics()
 viv_1_0_n1.plot_all_timeseries()
 viv_1_0_n1.plot_section_timeseries()
@@ -162,6 +232,20 @@ viv_1_5_n1 = VIV_files("viv_1_5_n1.bin", starttimes=17.0, endtimes=23.4, title="
 viv_1_5_n1.write_statistics()
 viv_1_5_n1.plot_all_timeseries()
 viv_1_5_n1.plot_section_timeseries()
+
+runs = [viv_0_2_n1, viv_0_3_n1, viv_0_4_n1, viv_0_5_n1, viv_0_6_n1, viv_0_7_n1, viv_0_75_n1, viv_0_8_n1, viv_0_83_n1, viv_0_84_n1, viv_0_85_n1, viv_0_88_n1, viv_0_9_n1, viv_1_0_n1, viv_1_1_n1, viv_1_2_n1, viv_1_3_n1, viv_1_4_n1, viv_1_5_n1]
+
+speed_means = np.array([run.means["Speed - Mean"] for run in runs])
+X1_means = np.array([run.means["1X - Mean"] for run in runs])
+X2_means = np.array([run.means["2X - Mean"] for run in runs])
+X3_means = np.array([run.means["3X - Mean"] for run in runs])
+Y1_meanpeaks = np.array([run.meanpeaks["1Y - mean(peaks)"] for run in runs])
+Y2_meanpeaks = np.array([run.meanpeaks["2Y - mean(peaks)"] for run in runs])
+Y3_meanpeaks = np.array([run.meanpeaks["3Y - mean(peaks)"] for run in runs])
+
+fig, ax = plt.subplots()
+ax.plot(speed_means, Y2_meanpeaks)
+plt.show()
 
 
 """
--- utility.py
+++ utility.py
@@ -5,12 +5,12 @@ import matplotlib.pyplot as plt
 from settings import *
 from matplotlib_visualization import*
 from scipy.fft import fft, fftfreq, rfft, rfftfreq
-from scipy.signal import welch
+from scipy.signal import welch, find_peaks
 plot_scale(1, title=1.5)
 
 
 class VIV_files:
-    def __init__(self, filename, starttimes, endtimes, title):
+    def __init__(self, filename, starttimes, endtimes, title, samplingfreq=200):
         self.filename = filename
         self.measurements = APReader(path+filename)
         self.title = title
@@ -19,6 +19,8 @@ class VIV_files:
         self.means = {}
         self.std_devs = {}
         self.maxes = {}
+        self.meanpeaks = {}
+        self.sfreq = samplingfreq
         try:
             os.mkdir(towing_results)
         except FileExistsError:
@@ -50,13 +52,17 @@ class VIV_files:
         for i in range(1, self.measurements.numChannels):
             fig = plt.figure()
             ax = plt.subplot(111)
-            ax.plot(self.measurements.Channels[i].Time.data[int(self.starttimes*sampling_freq):int(self.endtimes*sampling_freq)], self.measurements.Channels[i].data[int(self.starttimes*sampling_freq):int(self.endtimes*sampling_freq)])
+            data = self.measurements.Channels[i].data[int(self.starttimes*self.sfreq):int(self.endtimes*self.sfreq)]
+            time = self.measurements.Channels[i].Time.data[int(self.starttimes*self.sfreq):int(self.endtimes*self.sfreq)]
+            peaks = find_peaks(data, height=np.mean(self.measurements.Channels[i].data[int(self.starttimes*self.sfreq):int(self.endtimes*self.sfreq)]))[0]
+            ax.plot(self.measurements.Channels[i].Time.data[int(self.starttimes*self.sfreq):int(self.endtimes*self.sfreq)], self.measurements.Channels[i].data[int(self.starttimes*self.sfreq):int(self.endtimes*self.sfreq)])
+            ax.scatter(time[peaks], data[peaks], color="red", alpha=0.5, s=25)
             fig.suptitle(self.title+"\n"+self.measurements.Channels[i].Name+" measurement - Utilized section")
             ax.set_xlabel("Time [s]")
             ax.set_ylabel(self.measurements.Channels[i].unit)
 
-            mean = np.mean(self.measurements.Channels[i].data[int(self.starttimes*sampling_freq):int(self.endtimes*sampling_freq)])
-            std_dev = np.std(self.measurements.Channels[i].data[int(self.starttimes*sampling_freq):int(self.endtimes*sampling_freq)], ddof=1)
+            mean = np.mean(self.measurements.Channels[i].data[int(self.starttimes*self.sfreq):int(self.endtimes*self.sfreq)])
+            std_dev = np.std(self.measurements.Channels[i].data[int(self.starttimes*self.sfreq):int(self.endtimes*self.sfreq)], ddof=1)
             ax.axhline(y=mean, label="Mean value \n ={} {}".format(round(self.means[self.measurements.Channels[i].Name+" - Mean"],2), self.measurements.Channels[i].unit), c="red")
             plt.legend(loc='upper right', bbox_to_anchor=(1.1, 1.05), fancybox=True, shadow=True)
             plt.tight_layout()
@@ -72,7 +78,7 @@ class VIV_files:
             f.write("Start time: {}\n".format(self.starttimes))
             f.write("End time: {}\n -------------------\n".format(self.endtimes))
             for i in range(1, self.measurements.numChannels):
-                data_section = self.measurements.Channels[i].data[int(self.starttimes*sampling_freq):int(self.endtimes*sampling_freq)]
+                data_section = self.measurements.Channels[i].data[int(self.starttimes*self.sfreq):int(self.endtimes*self.sfreq)]
                 f.write(self.measurements.Channels[i].Name+"\n -------------------\n")
                 mean = np.mean(data_section)
                 self.means[self.measurements.Channels[i].Name+" - Mean"] = mean
@@ -80,13 +86,15 @@ class VIV_files:
                 std_dev = np.std(data_section, ddof=1)
                 self.std_devs[self.measurements.Channels[i].Name+" - Std.dev"] = std_dev
                 self.maxes[self.measurements.Channels[i].Name+" - abs(max)"] = np.max(np.abs(data_section))
+                self.meanpeaks[self.measurements.Channels[i].Name+" - mean(peaks)"] = np.mean(data_section[find_peaks(data_section, height=mean)[0]])
                 f.write("Std.dev: {}\n".format(std_dev))
                 f.write("\n -------------------\n")
 
     def get_valid_measurements(self):
-        data = [channel.data[int(self.starttimes*sampling_freq):int(self.endtimes*sampling_freq)] for channel in self.measurements.Channels]
+        data = [channel.data[int(self.starttimes*self.sfreq):int(self.endtimes*self.sfreq)] for channel in self.measurements.Channels]
+        peaks = find_peaks(data)[0]
         name = [channel.Name for channel in self.measurements.Channels]
-        return name, data
+        return name, data, peaks
 
 
 
@@ -94,7 +102,7 @@ class VIV_files:
         name = np.array([channel.Name for channel in self.measurements.Channels])
         idx = np.where(name==name_fft)[0][0]
         timeseries = self.measurements.Channels[idx].data
-        dt = 1/sampling_freq
+        dt = 1/self.sfreq
         norm_timeseries = timeseries-np.mean(timeseries)
         yf = rfft(norm_timeseries)
         xf = rfftfreq(n=len(norm_timeseries), d=dt)
@@ -122,7 +130,7 @@ class VIV_files:
         idx = np.where(name == name_fft)[0][0]
         timeseries = self.measurements.Channels[idx].data
         norm_timeseries = timeseries-np.mean(timeseries)
-        f, Pxx = welch(norm_timeseries, fs=sampling_freq, return_onesided=True, scaling="density", nperseg=2048)
+        f, Pxx = welch(norm_timeseries, fs=self.sfreq, return_onesided=True, scaling="density", nperseg=2048)
         plt.plot(f, Pxx)
         #plt.ylim(0.5e-3, 10)
         plt.xlim(0, 10)
@@ -138,3 +146,20 @@ class VIV_files:
             plt.clf()
 
         return Pxx, f
+
+
+class Decay_test:
+    def __init__(self, filename, starttimes, endtimes, title):
+        self.filename = filename
+        self.measurements = APReader(path+filename)
+        self.title = title
+        self.starttimes = starttimes
+        self.endtimes = endtimes
+
+        try:
+            os.mkdir(towing_results)
+        except FileExistsError:
+            pass
+
+def ReducedVelocity(U, f, d):
+    return U/(f*d)
